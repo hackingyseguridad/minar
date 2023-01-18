48df3b03cec8bd1e8ac98ec541d7d87af2e6675b
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-18 14:08:00 +0100

    Update minar.sh

--- minar.sh
+++ minar.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 # script para iniciar minado ..
-# 
+# para inciar como proceso:  
+# $nohup sudo ./xmrig &
 # (c) hacking y seguridad .com 2023
 
 while :
 do
+  chmod 777 xmrig
   echo " Iniciando minado ... "
   echo "### www.hackingyseguridad.com ###"
   sleep 3
