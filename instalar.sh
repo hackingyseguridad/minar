bf718058b62d183a710f95b530d8d17ed77dcb63
hackingyseguridad <antonio_taboada@hotmail.es>
2023-01-25 11:39:27 +0100

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -3,6 +3,7 @@ echo
 echo instalando...
 echo
 wget https://github.com/xmrig/xmrig/releases/download/v6.18.1/xmrig-6.18.1-linux-x64.tar.gz -q -O xmrig.tar.gz
+tar -xzvf xmrig.tar.gz
 
 chmod 777 xmrig
 ./xmrig
