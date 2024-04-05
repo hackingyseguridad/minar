8d491762a34df164fbb6d588b0636bfd4c3f0b10
hackingyseguridad <antonio_taboada@hotmail.es>
2024-04-05 11:16:36 +0200

    Create mina.sh

---
+++ mina.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# (c) hacking y seguridad .com 2024
+
+while :
+do
+  chmod 777 xmrig
+  echo
+  echo "### www.hackingyseguridad.com ###"
+  echo " Iniciando minado ... "
+  echo
+  sleep 3
+  xmrig
+done
